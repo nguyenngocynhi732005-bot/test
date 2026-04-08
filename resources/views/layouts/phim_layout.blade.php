53018c37bd3d93addef5358bfc654e4720e818ec
huakimngannlt <huakimngan.nlt@gmail.com>
2026-04-08 09:56:36 +0700

    CODE

--- resources/views/layouts/phim_layout.blade.php
+++ resources/views/layouts/phim_layout.blade.php
@@ -100,6 +100,12 @@
              </div>
          </div>
      </main>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+     <script type="text/javascript" src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.10.25/js/dataTables.bootstrap4.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap4.min.css">
  </body>
 
  </html>
--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MovieAddController;
+use App\Http\Controllers\MovieManagementController;
 
-// Trần Thị Ngọc An
 Route::get('/', [App\Http\Controllers\MovieController::class, 'index']);
-Route::get('/phim', 'App\Http\Controllers\ViduLayoutController@phim')->name('phim.index');;
-Route::get('/phim/theloai/{id}', 'App\Http\Controllers\ViduLayoutController@theloai')->name('phim.theloai');
-
-
-// Nguyễn Ngọc Ý Nhi
-Route::get('/movie/create', 'App\Http\Controllers\MovieAddController@create')->name('movies.create');
-Route::post('/movie/store', 'App\Http\Controllers\MovieAddController@store')->name('movies.store');
+//Hứa Kim Ngân
+Route::get('/quanlyphim', [MovieManagementController::class, 'index'])->name('movie.index');
+Route::get('/quanlyphim/{id}', [MovieManagementController::class, 'show'])->name('movie.show');
+Route::delete('/quanlyphim/{id}', [MovieManagementController::class, 'destroy'])->name('movie.destroy');
